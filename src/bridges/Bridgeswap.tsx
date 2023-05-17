21f95b335ed6171a7e58eb8574401cc52d5b611a
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2023-05-17 16:21:31 +0200

    Update Bridgeswap.tsx

--- src/bridges/Bridgeswap.tsx
+++ src/bridges/Bridgeswap.tsx
@@ -4,7 +4,7 @@ const widgetConfig: WidgetConfig = {
   integrator: 'krusty',
   fromChain: 137,
   fromToken: '0x386C126Edf2013A0f146BDe8dF1b67D93e2ACE90',
-   logoURI: 'https://i.postimg.cc/T3DWSPJ5/IMG-20230509-182205.jpg',
+   logoURI.jpg: 'https://i.postimg.cc/T3DWSPJ5/IMG-20230509-182205.jpg',
   containerStyle: {
     border: '1px solid rgb(234, 234, 234)',
     borderRadius: '16px',
